dbe85192a1e0177b8c22a02a4b966e37841a6039
Yejia Hao <haoyejia@outlook.com>
2024-09-06 09:02:35 -0400

    Add table of content and fix figure position

--- GenerateLCAReport.py
+++ GenerateLCAReport.py
@@ -1,19 +1,6 @@
 import pandas
-# import reportlab
-# from reportlab.pdfgen import canvas
-# from reportlab.platypus.flowables import Flowable
-# from reportlab.lib.styles import ParagraphStyle as PS
-# from reportlab.platypus import SimpleDocTemplate, PageTemplate, Table, TableStyle, Paragraph, PageBreak, Spacer, Image
-# from reportlab.platypus.tableofcontents import TableOfContents
-# from reportlab.lib.units import inch
-# from reportlab.lib import colors
-# from reportlab.graphics.shapes import Drawing
-# from reportlab.graphics.charts.barcharts import VerticalBarChart
-#from reportlab.platypus.flowables import LinkInPDF
 import matplotlib.pyplot as plt
 import tikzplotlib
-# from io import BytesIO
-# from svglib.svglib import svg2rlg
 from pylatex import Command, Document, Section, Subsection, LongTable, Tabularx, Package, Figure, NoEscape
 import re
 
@@ -33,7 +20,13 @@ class GenerateLCAReport():
         self.elements = []
         self.pdfReport = Document("basic")
         self.pdfReport.preamble.append(Command('usepackage', ['geometry']))
+        self.pdfReport.preamble.append(Command('usepackage', ['hyperref']))
         self.pdfReport.preamble.append(Command('geometry', arguments='left=1in, right=1in, top=1in, bottom=1in'))
+
+        # Add table of Contents
+        self.pdfReport.append(Command('tableofcontents'))
+        self.pdfReport.append(Command('newpage'))
+
 
 
     
@@ -45,46 +38,32 @@ class GenerateLCAReport():
         #employerGeneralTable = self.generateEmployerGeneralDataFrame()
         #styles = reportlab.lib.styles.getSampleStyleSheet()
 
+        with self.pdfReport.create(Section("General Table")):
 
-        for year in yearRange:
-            print("Generating general table for year {}".format(year))
+            for year in yearRange:
+                print("Generating general table for year {}".format(year))
 
-            #self.elements.append(Paragraph("{YEAR} General Table".format(YEAR=year),style=styles['Title']))
-            with self.pdfReport.create(Section("{YEAR} General Table".format(YEAR=year))):
+                #self.elements.append(Paragraph("{YEAR} General Table".format(YEAR=year),style=styles['Title']))
+                with self.pdfReport.create(Subsection("{YEAR} General Table".format(YEAR=year))):
 
-                # Generate year general table
-                yearEmployerGeneralTable = self.countCategoryTable[(self.countCategoryTable["YEAR"] == (year))]
-                yearEmployerGeneralTable = yearEmployerGeneralTable[["YEAR", "EMPLOYER_NAME", "H1B_CER_MAJ_JOB_NUM","NEITHER_H1B_CER_MAJ_JOB_NUM",  "TOTAL"]]
-                yearEmployerGeneralTable = yearEmployerGeneralTable[yearEmployerGeneralTable["H1B_CER_MAJ_JOB_NUM"] >= self.jobNumLimit]
-                yearEmployerGeneralTable["EMPLOYER_NAME"] = yearEmployerGeneralTable["EMPLOYER_NAME"].apply(self.employerNameToParagraph)
-                yearEmployerGeneralTable = yearEmployerGeneralTable.sort_values(by=["H1B_CER_MAJ_JOB_NUM"], ascending=False)
+                    # Generate year general table
+                    yearEmployerGeneralTable = self.countCategoryTable[(self.countCategoryTable["YEAR"] == (year))]
+                    yearEmployerGeneralTable = yearEmployerGeneralTable[["YEAR", "EMPLOYER_NAME", "H1B_CER_MAJ_JOB_NUM","NEITHER_H1B_CER_MAJ_JOB_NUM",  "TOTAL"]]
+                    yearEmployerGeneralTable = yearEmployerGeneralTable[yearEmployerGeneralTable["H1B_CER_MAJ_JOB_NUM"] >= self.jobNumLimit]
+                    yearEmployerGeneralTable["EMPLOYER_NAME"] = yearEmployerGeneralTable["EMPLOYER_NAME"].apply(self.employerNameToParagraph)
+                    yearEmployerGeneralTable = yearEmployerGeneralTable.sort_values(by=["H1B_CER_MAJ_JOB_NUM"], ascending=False)
 
-                
-                # table = reportlab.platypus.Table([["Year", "Employer Name", self.majorName + " \n H-1B \n Certified", "Other", "Total"]] + yearEmployerGeneralTable.values.tolist())
-
-                # style = reportlab.platypus.TableStyle([
-                #     ('BACKGROUND', (0, 0), (-1, 0), reportlab.lib.colors.grey),
-                #     ('TEXTCOLOR', (0, 0), (-1, 0), reportlab.lib.colors.whitesmoke),
-                #     ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-                #     ('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-                #     ('FONTSIZE', (0, 0), (-1, 0), 14),
-                #     ('BOTTOMPADDING', (0, 0), (-1, 0), 12),
-                #     ('BACKGROUND', (0, 1), (-1, -1), reportlab.lib.colors.beige),
-                #     ('GRID', (0, 0), (-1, -1), 1, reportlab.lib.colors.black),
-                # ])
-                # table.setStyle(style)
-
-                # self.elements.append(table)
-                # self.elements.append(PageBreak())
-                
-                with self.pdfReport.create(LongTable(r"c|p{20em}|p{5em}|c|c")) as table:
-                    table.add_hline()
-                    table.add_row(["Year", "Employer Name", self.majorName + " \n H-1B \n Certified", "Other", "Total"])  # Add column names
-                    table.add_hline()
-
-                    for index, row in yearEmployerGeneralTable.iterrows():
-                        table.add_row(row)
+                    
+                    with self.pdfReport.create(LongTable(r"c|p{20em}|p{5em}|c|c")) as table:
                         table.add_hline()
+                        table.add_row(["Year", "Employer Name", self.majorName + " \n H-1B \n Certified", "Other", "Total"])  # Add column names
+                        table.add_hline()
+
+                        for index, row in yearEmployerGeneralTable.iterrows():
+                            table.add_row(row)
+                            table.add_hline()
+
+                self.pdfReport.append(Command('newpage'))
 
    
     def drawDetailedPlots(self, ax, num, fontSize, years, primaryData, primaryDataColor, primaryDataName, secondaryData, secondaryDataColor, secondaryDataName):
@@ -96,17 +75,6 @@ class GenerateLCAReport():
         ax[num].set_xticks(years.values.tolist())
         ax[num].legend([primaryDataName, secondaryDataName],fontsize=fontSize)
 
-        # Save the plot to a BytesIO object
-        # img_buffer = BytesIO()
-        # plt.savefig(img_buffer, format='svg')
-        # plt.close(fig)
-        # img_buffer.seek(0)
-        # drawing = svg2rlg(img_buffer)
-        # return drawing
-        
-        
-
-        
 
     def generateEmployerDetailedPages(self):
         # styles = reportlab.lib.styles.getSampleStyleSheet()
@@ -122,80 +90,56 @@ class GenerateLCAReport():
         # limit numbers of employers for faster debug
         validEmployers = validEmployers[:10]
             
-        
-        # Create detailed pages only for those employers who meet the criteria
-        for name in validEmployers:
+        with self.pdfReport.create(Section("Employer Detail")):
 
-        # for index, name in self.employerList.items():
-            dataTable = self.countCategoryTable[self.countCategoryTable["EMPLOYER_NAME"] == name]
+            # Create detailed pages only for those employers who meet the criteria
+            for name in validEmployers:
 
-        
-        #     if ((dataTable["H1B_CER_MAJ_JOB_NUM"] < self.jobNumLimit).all().all()):
-        #         continue
-            
-          
+                dataTable = self.countCategoryTable[self.countCategoryTable["EMPLOYER_NAME"] == name]
 
-            print("Generating detailed page for {}".format(name))
+                print("Generating detailed page for {}".format(name))
 
-            fig, ax = plt.subplots(nrows=3, figsize=(5.3,5.5))
+                fig, ax = plt.subplots(nrows=3, figsize=(5.3,5.5))
 
-            self.drawDetailedPlots(ax=ax, num=0, fontSize=7,
-                                    years=dataTable["YEAR"],
-                                    primaryData=dataTable["H1B_CER_MAJ_JOB_NUM"],
-                                    primaryDataColor="#77AC30",
-                                    primaryDataName="{} Certified H-1B Jobs".format(self.majorName),
-                                    secondaryData=dataTable["NEITHER_H1B_CER_MAJ_JOB_NUM"],
-                                    secondaryDataColor="tab:gray",
-                                    secondaryDataName="Other Jobs")
-            
-            
+                self.drawDetailedPlots(ax=ax, num=0, fontSize=7,
+                                        years=dataTable["YEAR"],
+                                        primaryData=dataTable["H1B_CER_MAJ_JOB_NUM"],
+                                        primaryDataColor="#77AC30",
+                                        primaryDataName="{} Certified H-1B Jobs".format(self.majorName),
+                                        secondaryData=dataTable["NEITHER_H1B_CER_MAJ_JOB_NUM"],
+                                        secondaryDataColor="tab:gray",
+                                        secondaryDataName="Other Jobs")
+                
+                
 
-            self.drawDetailedPlots(ax=ax, num=1, fontSize=7,
-                                    years=dataTable["YEAR"],
-                                    primaryData=dataTable["H1B_JOB_NUM"],
-                                    primaryDataColor="#82B0D2",
-                                    primaryDataName="H-1B Visa",
-                                    secondaryData=dataTable["NOT_H1B_JOB_NUM"],
-                                    secondaryDataColor="tab:gray",
-                                    secondaryDataName="Other Visa")
-            
-            self.drawDetailedPlots(ax=ax, num=2, fontSize=7,
-                                    years=dataTable["YEAR"],
-                                    primaryData=dataTable["CER_JOB_NUM"],
-                                    primaryDataColor="#82B0D2",
-                                    primaryDataName="Certified",
-                                    secondaryData=dataTable["NOT_CER_JOB_NUM"],
-                                    secondaryDataColor="tab:gray",
-                                    secondaryDataName="Not Certified")
-            
-            detailedImgName = "{filename}.pdf".format(filename=(self.employerNameToLabel(name) + "_detailed"))
-                                                      
-            plt.savefig((tempImgPath + detailedImgName), format='pdf')
-            
-            # return
+                self.drawDetailedPlots(ax=ax, num=1, fontSize=7,
+                                        years=dataTable["YEAR"],
+                                        primaryData=dataTable["H1B_JOB_NUM"],
+                                        primaryDataColor="#82B0D2",
+                                        primaryDataName="H-1B Visa",
+                                        secondaryData=dataTable["NOT_H1B_JOB_NUM"],
+                                        secondaryDataColor="tab:gray",
+                                        secondaryDataName="Other Visa")
+                
+                self.drawDetailedPlots(ax=ax, num=2, fontSize=7,
+                                        years=dataTable["YEAR"],
+                                        primaryData=dataTable["CER_JOB_NUM"],
+                                        primaryDataColor="#82B0D2",
+                                        primaryDataName="Certified",
+                                        secondaryData=dataTable["NOT_CER_JOB_NUM"],
+                                        secondaryDataColor="tab:gray",
+                                        secondaryDataName="Not Certified")
+                
+                detailedImgName = "{filename}.pdf".format(filename=(self.employerNameToLabel(name) + "_detailed"))
+                                                        
+                plt.savefig((tempImgPath + detailedImgName), format='pdf')
 
-            # table = reportlab.platypus.Table([["Year", self.majorName + " H-1B Certified", "H-1B", "Certified" , "Total"]] + dataTable[["YEAR", "H1B_CER_MAJ_JOB_NUM", "H1B_JOB_NUM","CER_JOB_NUM","TOTAL"]].values.tolist())
-
-            # self.elements.append(Spacer(1,8))
-            # self.elements.append(table)
-
-            # tablestyle = reportlab.platypus.TableStyle([
-            #     ('BACKGROUND', (0, 0), (-1, 0), reportlab.lib.colors.grey),
-            #     ('TEXTCOLOR', (0, 0), (-1, 0), reportlab.lib.colors.whitesmoke),
-            #     ('ALIGN', (0, 0), (-1, -1), 'CENTER'),
-            #     #('FONTNAME', (0, 0), (-1, 0), 'Helvetica-Bold'),
-            #     ('FONTSIZE', (0, 0), (-1, 0), 10),
-            #     ('BOTTOMPADDING', (0, 0), (-1, 0), 10),
-            #     ('BACKGROUND', (0, 1), (-1, -1), reportlab.lib.colors.white),
-            #     ('GRID', (0, 0), (-1, -1), 1, reportlab.lib.colors.black),
-            # ])
-            # table.setStyle(tablestyle)
-
-            # self.elements.append(PageBreak())
-            with self.pdfReport.create(Section(name + ":")):
-                with self.pdfReport.create(Figure(position="h")) as plot:
-                    plot.add_image(filename=(tempImgPath + detailedImgName), width=NoEscape(r"0.9\textwidth"))
-        return
+                with self.pdfReport.create(Subsection(name + ":")):
+                    self.pdfReport.append("Visa Data Chart:")
+                    with self.pdfReport.create(Figure(position="htbp")) as plot:
+                        plot.add_image(filename=(tempImgPath + detailedImgName), width=NoEscape(r"0.9\textwidth"))
+                
+                self.pdfReport.append(Command('newpage'))
 
     
     def countCategoryOfCombinedData(self, combinedData):
@@ -240,8 +184,8 @@ class GenerateLCAReport():
 
     def export(self, name):
         print("Exporting PDF")
-        self.pdfReport.generate_pdf(name, clean_tex=False)
-        # self.pdfReport.generate_tex(name)
+        # self.pdfReport.generate_pdf(name, clean_tex=False)
+        self.pdfReport.generate_tex(name)
 
     def employerNameToLabel(self, name):
         return "" + str(re.sub("[^A-Za-z0-9]","",name))
